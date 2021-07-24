e58c1ebb4810a7bbf7da7ca2abfd581f1a48e6ca
KaraivanOK <porohvernivk@gmail.com>
2021-07-24 18:05:33 +0300

    Домашнее задание 2.

--- HW2/Escape_sequences.py
+++ HW2/Escape_sequences.py
@@ -1,6 +1,6 @@
 from prettytable import PrettyTable # подключаем библиотеку pretty table через Settings-Python Interpreter
 x = PrettyTable()
 x.add_column("КОД", [r'\a', r'\b', r'\n', r'\t', r'\\',  r'\"', r"\'"]) # Первая колонка  с шапкой и данными.
-x.add_column("Описание", ['Bell (alert)', 'Backspace', 'New line', 'Horizontal tab', r"Backslah \\",
-'Double quotation mark "', " Single quotation mark '"]) #Вторая колонка с шапукой и данными.
+x.add_column("Описание", ['Bell (alert)', 'Backspace', 'New line', 'Horizontal tab', "Backslah \\",
+'Double quotation mark \"', " Single quotation mark \'"]) #Вторая колонка с шапукой и данными.
 print(x)  # Печатаем таблицу
