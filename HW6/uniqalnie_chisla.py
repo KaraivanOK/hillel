f9c9153f1b03f9466d819b823157e72c57779570
KaraivanOK <porohvernivk@gmail.com>
2021-08-09 22:55:59 +0300

    Домашнее задание 6.

--- HW6/uniqalnie_chisla.py
+++ HW6/uniqalnie_chisla.py
@@ -1,8 +1,8 @@
 import random  # Импортируем модуль random.
 
 # Создаём два сгенерированных списка.
-list_1 = [random.randint(0, 100) for i in range(0, 10)]
-list_2 = [random.randint(0, 100) for i in range(0, 10)]
+list_1 = [random.randint(0, 10) for i in range(0, 10)]
+list_2 = [random.randint(0, 10) for i in range(0, 10)]
 # Создаём список из элементов, которые не повторяются ни в одном из сгенерированных списков и находим длину этого
 # списка.
 print(len([x for x in (list_1 + list_2) if (list_1 + list_2).count(x) == 1]))
