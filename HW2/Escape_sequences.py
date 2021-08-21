3fc5eb1672bb0ddc4c0f60072c7f64efecf861e5
KaraivanOK <porohvernivk@gmail.com>
2021-08-21 21:09:55 +0300

    Домашнее задание 8.

--- HW2/Escape_sequences.py
+++ HW2/Escape_sequences.py
@@ -1,6 +1,10 @@
-from prettytable import PrettyTable # подключаем библиотеку pretty table через Settings-Python Interpreter
+from prettytable import PrettyTable  # подключаем библиотеку pretty table через Settings-Python Interpreter
+
 x = PrettyTable()
-x.add_column("КОД", [r'\a', r'\b', r'\n', r'\t', r'\\',  r'\"', r"\'"]) # Первая колонка  с шапкой и данными.
+# Первая колонка  с шапкой и данными.
+x.add_column("КОД", [r'\a', r'\b', r'\n', r'\t', r'\\', r'\"', r"\'"])
+# Вторая колонка с шапкой и данными.
 x.add_column("Описание", ['Bell (alert)', 'Backspace', 'New line', 'Horizontal tab', "Backslah \\",
-'Double quotation mark \"', " Single quotation mark \'"]) #Вторая колонка с шапукой и данными.
+                          'Double quotation mark \"',
+                          " Single quotation mark \'"])
 print(x)  # Печатаем таблицу
---
+++ HW9/func_sort.py
@@ -0,0 +1,37 @@
+import random
+
+M = int(input("Введите любое целое положительное число больше 5!: "))
+massiv1 = []
+while M < 5:
+    print("Вы ввели число меньше 5! Повторите попытку.")
+    M = int(input("Введите любое целое положительное число больше 5!: "))
+else:
+    massiv = [[random.randint(1, 50) for i in range(M)] for j in range(M)]
+print()
+print("До сортировки:")
+for i in range(M):
+    s = 0
+    for j in range(M):
+        s += massiv[j][i]
+    massiv1.append(s)
+massiv2 = massiv + [massiv1]
+for j in range(M + 1):
+    for i in range(M):
+        print("%4d" % massiv2[j][i], end=' ')
+    print()
+print()
+print("После сортировки:")
+k = M - 1
+while k > 0:
+    ind = 0
+    for i in range(k + 1):
+        if massiv2[M][i] > massiv2[M][ind]:
+            ind = i
+    for j in range(M + 1):
+        massiv2[j][ind], massiv2[j][k] = massiv2[j][k], massiv2[j][ind]
+    k -= 1
+for j in range(M + 1):
+    for i in range(M):
+        print("%4d" % massiv2[j][i], end=' ')
+    print()
+print()
