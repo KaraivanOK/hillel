5048903f04d6234813cb8d059c42827a51e17a07
KaraivanOK <porohvernivk@gmail.com>
2021-08-24 16:27:10 +0300

    Домашнее задание 10.

--- HW10/reverse_number.py
+++ HW10/reverse_number.py
@@ -1,16 +1,11 @@
-def reverse_it(n):
-    if n < 10:
-        return n  # выход из рекурсии
-    d = n % 10
-    r = reverse_it(n // 10)
-    dd = 1  # Количество вхождений в рекурсию.
-    r2 = r
-    if r2 > 10:
-        r2 //= 10
-        dd += 1
-    return d * (10 ** dd) + r
+def reverse_it(n, i):
+    if n == 0:
+        return i
+    else:
+        return reverse_it(n // 10, i * 10 + n % 10)
+
 
 print("Ввод: \n 179 ")
 print('-' * 10)
 print("Вывод:")
-print(reverse_it(179))
+print(reverse_it(179, 0))
