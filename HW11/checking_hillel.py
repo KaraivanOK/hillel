8c223d4b76d3a46892d4b9fbb8cb0053f8e8d222
KaraivanOK <porohvernivk@gmail.com>
2021-09-01 03:38:22 +0300

    Домашнее задание 11.

--- HW11/checking_hillel.py
+++ HW11/checking_hillel.py
@@ -37,3 +37,31 @@ print('+' * 100, '\n')
 
 print(help(hillel.func_module), '\n')
 print(' ' * 50, 'CHECKING func_module\n')
+my_list4 = [0, 1, 2, 3, 4, 5]
+num = 9
+a = 1
+b = 100
+print("1.Проверим есть ли в списке 2 числа, сума которых еквивалентна числу переданому 2м параметром.\n Ввод:\n",
+      my_list4, "\n", num, "\n Вывод:\n", hillel.two_sum(my_list4, num))
+print("2.Возьмём список с числами и возведём каждое число списка в квадрат.\n Ввод:\n",
+      my_list4, "\n Вывод:\n", hillel.list_numbers_squared(my_list4))
+print("3.Найдём простые числа в дипазоне заданным 2мя аргументами чисел.\n Ввод:\n",
+      a, "\n", b, "\n Вывод:")
+print(hillel.prime_number(a, b))
+print('+' * 100, '\n')
+
+print(help(hillel.sequence_sorting_module), '\n')
+print(' ' * 50, 'CHECKING sequence_sorting_module\n')
+print("1.Выведем заданное количество членов треугольной последовательности.\n Ввод:\n", 5, "\n Вывод:")
+print(hillel.triangle_sequence(5))
+print("2.Определим количество последовательностей из 0 и 1, где два нуля не стоят рядом! \nВвод:"
+      " \nколичество нулей = 2 количество единиц = 2 \nВывод: \n", hillel.without_00(2, 2))
+print("3.Развернём число! \nВвод: \n 179 \nВывод:\n", hillel.reverse_it(179))
+print("4.Отсортируем матрицу!")
+print(hillel.func_sort_matrix([
+    [17, 38, 1, 25, 27, 1],
+    [28, 21, 17, 6, 45, 44],
+    [24, 36, 9, 2, 39, 1],
+    [21, 18, 23, 46, 19, 22],
+    [10, 14, 39, 5, 9, 31],
+    [16, 26, 3, 32, 24, 34]], 6))
