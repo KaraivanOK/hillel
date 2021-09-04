47351253a1e80241234c4695ee9778da889f7be0
KaraivanOK <porohvernivk@gmail.com>
2021-09-04 13:41:38 +0300

    Домашнее задание 12.

---
+++ HW12/test.py
@@ -0,0 +1,9 @@
+import concatenation
+
+print(help(concatenation))
+print()
+print("Результаты проверки функции:")
+print(concatenation.concantenation(10, 'пока'))
+print(concatenation.concantenation(10, 20))
+print(concatenation.concantenation('Привет', 20))
+print(concatenation.concantenation('Привет', 'пока'))
