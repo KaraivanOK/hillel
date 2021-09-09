31073700db0c61efd9e9bb320bbe1b7332d72c28
KaraivanOK <porohvernivk@gmail.com>
2021-09-09 13:15:03 +0300

    Домашнее задание 12.

--- HW12/my_error_class.py
+++ HW12/my_error_class.py
@@ -9,11 +9,11 @@ try:
     a = int(a)
     if a < 0:
         raise NegativeNumber("Вы ввели отрицательное число!")
-    elif a ==0:
+    elif a == 0:
         raise NegativeNumber("Вы ввели ноль!")
 except ValueError:
     print("Вы ввели не число!")
 except NegativeNumber as mr:
     print(mr)
 else:
-    print("Ура! Вы ввели положительное число: ",a)
+    print("Ура! Вы ввели положительное число: ", a)
